baa261e3fc5c54330f11adf831a31730b3d2741b
elkhunter187 <elkhunter187@hotmail.com>
2021-08-20 17:16:30 -0600

    assignment 2 selecting elements

---
+++ week1/week1.js
@@ -0,0 +1,4 @@
+//console.log(document)
+//console.log("have a good day")
+
+document.getElementById("myHeader").innerText = "This is My Header";
--- week2/week1.js
+++
@@ -1,2 +0,0 @@
-console.log(document)
-console.log("have a good day")
