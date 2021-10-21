08beb3f92c7186a90c1f865b3508ff620e37af50
elkhunter187 <elkhunter187@hotmail.com>
2021-10-20 20:43:15 -0600

    added index.html and index.js to rep

---
+++ week3/index.js
@@ -0,0 +1,15 @@
+var myH1 = document.createElement("h1");
+
+myH1.textContent = "HELLO WORLD";
+
+document.body.append(myH1);
+
+for (i = 0; i < 10; i++) {
+
+    var myH1 = document.createElement("h1");
+
+    myH1.textContent = "HELLO WORLD";
+    
+    document.body.append(myH1);
+
+}
